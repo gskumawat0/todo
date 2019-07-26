4032409461c6f032f1e8159975e65d050f3bea49
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2019-07-26 11:57:51 +0530

    commit build directory

---
+++ build/precache-manifest.6704589fd79a0eb132ede66a8dfb695c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3479a39abb21d2fdc0e87d10dad60283",
+    "url": "/gskumawat0/todo/index.html"
+  },
+  {
+    "revision": "29a1333072a80cc9aae0",
+    "url": "/gskumawat0/todo/static/css/main.cbe5a252.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/gskumawat0/todo/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "29a1333072a80cc9aae0",
+    "url": "/gskumawat0/todo/static/js/main.f949ba21.chunk.js"
+  },
+  {
+    "revision": "8ffcb654751543f350b5",
+    "url": "/gskumawat0/todo/static/js/runtime~main.78ed19e3.js"
+  }
+]);
